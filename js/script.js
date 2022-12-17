85733745c192ba3b9d2f09fd80b359697cf585f5
LucaFaedda <lucakarl91@gmail.com>
2022-12-18 00:01:35 +0100

    modificato input

--- js/script.js
+++ js/script.js
@@ -8,18 +8,25 @@ const {
 createApp({
     data(){
         return{
-            name: '',
-            surname: '',
+            name: null,
+            surname: null,
+            name_surname: null,
             class_style: 'text-danger',
             btn_class: "btn-primary btn",
             carlino: false
         }
     },
     methods: {
+        welcome: function(){
+            this.name_surname = this.name + ' ' + this.surname
+        },
         carlinoNatale: function(){
             this.carlino = true
-        }
+    
+            this.welcome
+        },
     },
+
 
 
 }).mount('#app')
