3f9d224c97c362a3737fa5545778e004721a88fb
LucaFaedda <lucakarl91@gmail.com>
2022-12-17 23:17:23 +0100

    aggiunto bonus

--- js/script.js
+++ js/script.js
@@ -3,13 +3,23 @@ const {
     createApp
 } = Vue;
 
+//import image from "./img/carlino.jpg"
+
 createApp({
     data(){
         return{
             name: '',
-            surname: ''
+            surname: '',
+            class_style: 'text-danger',
+            btn_class: "btn-primary btn",
+            carlino: false
         }
-    }
+    },
+    methods: {
+        carlinoNatale: function(){
+            this.carlino = true
+        }
+    },
 
 
 }).mount('#app')
